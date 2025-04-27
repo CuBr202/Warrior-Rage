ff234b43cfebe12ca796f699634d2d9877d9dda1
CuBr202 <2801307739@qq.com>
2025-04-27 09:03:35 +0800

    Update IRage.java

--- src/main/java/com/tiviacz/warriorrage/capability/IRage.java
+++ src/main/java/com/tiviacz/warriorrage/capability/IRage.java
@@ -3,11 +3,11 @@ package com.tiviacz.warriorrage.capability;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.player.Player;
 
-public interface IRage
-{
+public interface IRage {
     void startRage();
 
     boolean canStartRage();
+
     int getRemainingRageDuration();
 
     int getCurrentKillCount();
@@ -17,10 +17,25 @@ public interface IRage
     void decreaseRageDuration();
 
     void removeRageEffects();
+
     void refreshRageDuration();
+
+    float getHurtAmount();
+
+    void addHurtAmount(float s);
+
+    void setHurtAmount(float s);
+
+    float getDamageAmount();
+
+    void addDamageAmount(float s);
+
+    void setDamageAmount(float s);
+
     void setKillCount(int count);
 
     void setRageDuration(int time);
+
     void synchronise();
 
     void synchroniseToOthers(Player player);
