f689f8b19a53986085fed97dfbd5bb6c06e1b230
CuBr202 <2801307739@qq.com>
2025-04-27 09:02:40 +0800

    Update WarriorRageConfig.java

--- src/main/java/com/tiviacz/warriorrage/WarriorRageConfig.java
+++ src/main/java/com/tiviacz/warriorrage/WarriorRageConfig.java
@@ -7,111 +7,147 @@ import net.minecraftforge.fml.config.ModConfig;
 import org.apache.commons.lang3.tuple.Pair;
 
 @Mod.EventBusSubscriber(modid = WarriorRage.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class WarriorRageConfig
-{
-    public static class Server
-    {
-        public final ForgeConfigSpec.IntValue minimalKillCount;
-        public final ForgeConfigSpec.IntValue rageDuration;
-        public final ForgeConfigSpec.IntValue maxKillCountCap;
-        public final ForgeConfigSpec.IntValue killIntervalBetweenNextBonus;
-        public final ForgeConfigSpec.DoubleValue bonusDamage;
-        public final ForgeConfigSpec.BooleanValue enableFireDamage;
-        public final ForgeConfigSpec.IntValue fireDamageRequiredKillCount;
+public class WarriorRageConfig {
+        public static class Server {
+                public final ForgeConfigSpec.IntValue minimalKillCount;
+                public final ForgeConfigSpec.IntValue rageDuration;
+                public final ForgeConfigSpec.IntValue maxKillCountCap;
+                public final ForgeConfigSpec.IntValue killIntervalBetweenNextBonus;
+                public final ForgeConfigSpec.DoubleValue bonusDamage;
+                public final ForgeConfigSpec.DoubleValue bonusAttackSpeed;
+                public final ForgeConfigSpec.BooleanValue enableFireDamage;
 
-        Server(final ForgeConfigSpec.Builder builder)
-        {
-            builder.comment("Server config settings")
-                    .push("server");
+                public final ForgeConfigSpec.BooleanValue enableFireParticle;
+                public final ForgeConfigSpec.BooleanValue enableRageWhenHurt;
+                public final ForgeConfigSpec.DoubleValue hurtAmountCountAsKill;
+                public final ForgeConfigSpec.IntValue maximalKillCountPerHurt;
+                public final ForgeConfigSpec.BooleanValue enableRageWhenDamage;
+                public final ForgeConfigSpec.DoubleValue damageAmountCountAsKill;
+                public final ForgeConfigSpec.IntValue maximalKillCountPerDamage;
 
-            minimalKillCount = builder
-                    .comment("Minimal kill count for rage to enable")
-                    .defineInRange("minimalKillCount", 3, 0, 1000);
+                public final ForgeConfigSpec.IntValue fireDamageRequiredKillCount;
 
-            rageDuration = builder
-                    .comment("In Seconds")
-                    .defineInRange("rageDuration", 20, 0, 1000);
+                Server(final ForgeConfigSpec.Builder builder) {
+                        builder.comment("Server config settings")
+                                        .push("server");
 
-            maxKillCountCap = builder
-                    .comment("Max kill count for rage bonus damage")
-                    .defineInRange("maxKillCountCap", 20, 0, 1000);
+                        minimalKillCount = builder
+                                        .comment("Minimal kill count for rage to enable")
+                                        .defineInRange("minimalKillCount", 3, 0, 1000);
 
-            killIntervalBetweenNextBonus = builder
-                    .comment("Number of kills, which will multiply the bonus damage eg. 5 means, every 5 kills attack damage will be increased by bonusDamage value")
-                    .defineInRange("killIntervalBetweenNextBonus", 4, 1, 1000);
+                        rageDuration = builder
+                                        .comment("In Seconds")
+                                        .defineInRange("rageDuration", 20, 0, 1000);
 
-            bonusDamage = builder
-                    .comment("Bonus damage per 4 kills in a row")
-                    .defineInRange("bonusDamage", 0.5D, 0.01D, 10.0D);
+                        maxKillCountCap = builder
+                                        .comment("Max kill count for rage bonus damage")
+                                        .defineInRange("maxKillCountCap", 20, 0, 1000);
 
-            enableFireDamage = builder
-                    .comment("Enable Fire Damage")
-                    .define("enableFireDamage", true);
+                        killIntervalBetweenNextBonus = builder
+                                        .comment("Number of kills, which will multiply the bonus damage eg. 5 means, every 5 kills attack damage will be increased by bonusDamage value")
+                                        .defineInRange("killIntervalBetweenNextBonus", 4, 1, 1000);
 
-            fireDamageRequiredKillCount = builder
-                    .comment("Required minimal kill count for fire damage to apply")
-                    .defineInRange("fireDamageRequiredKillCount", 20, 0, 1000);
+                        bonusDamage = builder
+                                        .comment("Bonus damage percentage per 4 kills in a row")
+                                        .defineInRange("bonusDamage", 0.03D, 0.01D, 10.0D);
 
-            builder.pop();
+                        bonusAttackSpeed = builder
+                                        .comment("Bonus attack speed per 4 kills in a row")
+                                        .defineInRange("bonusAttackSpeed", 0.05D, 0.01D, 10.0D);
+
+                        enableFireDamage = builder
+                                        .comment("Enable Fire Damage")
+                                        .define("enableFireDamage", true);
+
+                        enableFireParticle = builder
+                                        .comment("Enable Fire Particle")
+                                        .define("enableFireParticle", true);
+
+                        enableRageWhenHurt = builder
+                                        .comment("Enable rage accumulation when taking damage.")
+                                        .define("enableRageWhenHurt", true);
+
+                        hurtAmountCountAsKill = builder
+                                        .comment("How many damage taken can count as one kill.")
+                                        .defineInRange("hurtAmountCountAsKill", 20.0, 1.0, 10000.0);
+
+                        maximalKillCountPerHurt = builder
+                                        .comment("No matter how many damage is taken once, the count that are taken as kills in a single time cannot exceed this value.")
+                                        .defineInRange("maximalKillCountPerHurt", 1, 1, 20);
+
+                        enableRageWhenDamage = builder
+                                        .comment("Enable rage accumulation when dealing damage.")
+                                        .define("enableRageWhenDamage", true);
+
+                        damageAmountCountAsKill = builder
+                                        .comment("How many damage dealt can count as one kill.")
+                                        .defineInRange("damageAmountCountAsKill", 100.0, 1.0, 10000.0);
+
+                        maximalKillCountPerDamage = builder
+                                        .comment("No matter how many damage is dealt once, the count that are taken as kills in a single time cannot exceed this value.")
+                                        .defineInRange("maximalKillCountPerDamage", 1, 1, 20);
+
+                        fireDamageRequiredKillCount = builder
+                                        .comment("Required minimal kill count for fire damage to apply")
+                                        .defineInRange("fireDamageRequiredKillCount", 20, 0, 1000);
+
+                        builder.pop();
+                }
         }
-    }
 
-    public static class Client
-    {
-        public final ForgeConfigSpec.BooleanValue renderRageBar;
-        public final ForgeConfigSpec.BooleanValue renderRageIcon;
-        public final ForgeConfigSpec.IntValue offsetX;
-        public final ForgeConfigSpec.IntValue offsetY;
+        public static class Client {
+                public final ForgeConfigSpec.BooleanValue renderRageBar;
+                public final ForgeConfigSpec.BooleanValue renderRageIcon;
+                public final ForgeConfigSpec.IntValue offsetX;
+                public final ForgeConfigSpec.IntValue offsetY;
 
-        Client(final ForgeConfigSpec.Builder builder)
-        {
-            builder.comment("Client-only settings")
-                    .push("client");
+                Client(final ForgeConfigSpec.Builder builder) {
+                        builder.comment("Client-only settings")
+                                        .push("client");
 
-            renderRageBar = builder
-                    .comment("Render Rage Bar on experience bar")
-                    .define("renderRageBar", true);
+                        renderRageBar = builder
+                                        .comment("Render Rage Bar on experience bar")
+                                        .define("renderRageBar", true);
 
-            renderRageIcon = builder
-                    .comment("Render Rage Icon next to player's Hotbar")
-                    .define("renderRageIcon", true);
+                        renderRageIcon = builder
+                                        .comment("Render Rage Icon next to player's Hotbar")
+                                        .define("renderRageIcon", true);
 
-            offsetX = builder
-                    .comment("Negative offsets to left side, positive to right")
-                    .defineInRange("offsetX", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+                        offsetX = builder
+                                        .comment("Negative offsets to left side, positive to right")
+                                        .defineInRange("offsetX", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
 
-            offsetY = builder
-                    .comment("Negative offsets to up, positive to down")
-                    .defineInRange("offsetY", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+                        offsetY = builder
+                                        .comment("Negative offsets to up, positive to down")
+                                        .defineInRange("offsetY", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
 
-            builder.pop();
+                        builder.pop();
+                }
         }
-    }
 
-    //COMMON
-    private static final ForgeConfigSpec serverSpec;
-    public static final Server SERVER;
+        // COMMON
+        private static final ForgeConfigSpec serverSpec;
+        public static final Server SERVER;
 
-    static {
-        final Pair<Server, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Server::new);
-        serverSpec = specPair.getRight();
-        SERVER = specPair.getLeft();
-    }
+        static {
+                final Pair<Server, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Server::new);
+                serverSpec = specPair.getRight();
+                SERVER = specPair.getLeft();
+        }
 
-    //CLIENT
-    private static final ForgeConfigSpec clientSpec;
-    public static final Client CLIENT;
+        // CLIENT
+        private static final ForgeConfigSpec clientSpec;
+        public static final Client CLIENT;
 
-    static {
-        final Pair<Client, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Client::new);
-        clientSpec = specPair.getRight();
-        CLIENT = specPair.getLeft();
-    }
+        static {
+                final Pair<Client, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Client::new);
+                clientSpec = specPair.getRight();
+                CLIENT = specPair.getLeft();
+        }
 
-    //Register
-    public static void register(final ModLoadingContext context)
-    {
-        context.registerConfig(ModConfig.Type.SERVER, serverSpec);
-        context.registerConfig(ModConfig.Type.CLIENT, clientSpec);
-    }
+        // Register
+        public static void register(final ModLoadingContext context) {
+                context.registerConfig(ModConfig.Type.SERVER, serverSpec);
+                context.registerConfig(ModConfig.Type.CLIENT, clientSpec);
+        }
 }
